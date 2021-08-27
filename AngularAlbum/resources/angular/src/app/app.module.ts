57f2f757f2844c78a14597b3ef085d5810b33af1
uHs13 <heitorhenriquedias@gmail.com>
2021-08-27 16:28:10 -0300

    163027082021

---
+++ AngularAlbum/resources/angular/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatCardModule} from '@angular/material/card';
+//import {MatButtonModule, MatDialog } from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatBadgeModule} from '@angular/material/badge';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    
+    MatButtonModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatDialogModule,
+    MatChipsModule,
+    MatBadgeModule
+  ],
+  exports: [
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ AngularAlbum/resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>AngularAlbum</title>
+    <base href="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="{{ asset('js/favicon.ico') }}">
+    <link rel="stylesheet" href="{{ asset('js/styles.e63a7758108cdabb8aae.css') }}">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"><link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body>
+    <app-root></app-root>
+    <script src="{{ asset('js/main.6f5df48f20900e6e4405.js') }}" defer></script>
+    <script src="{{ asset('js/polyfills.a809672107b8756266fb.js') }}" defer></script>
+    <script src="{{ asset('js/runtime.f7791ee12da892013155.js') }}" defer></script>
+</body>
+
+</html>
